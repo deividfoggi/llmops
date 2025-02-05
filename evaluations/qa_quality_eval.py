96f58ac67883ad19d14fcf1dda95eaa36c7dad76
deividfoggi <dev1@mngenvmcap380802.onmicrosoft.com>
2025-02-05 15:49:59 -0300

    Refactor token tracking to separate evaluator logic and add unit tests

--- evaluations/qa_quality_eval.py
+++ evaluations/qa_quality_eval.py
@@ -7,16 +7,7 @@ from promptflow.client import PFClient
 from promptflow.core import AzureOpenAIModelConfiguration
 from promptflow.evals.evaluate import evaluate
 from promptflow.evals.evaluators import RelevanceEvaluator, FluencyEvaluator, GroundednessEvaluator, CoherenceEvaluator
-
-class TokenTrackingEvaluator:
-    def __init__(self, evaluator):
-        self.evaluator = evaluator
-        self.total_tokens = 0
-
-    def evaluate(self, *args, **kwargs):
-        result = self.evaluator.evaluate(*args, **kwargs)
-        self.total_tokens += result['usage']['total_tokens']
-        return result
+from evaluations.token_usage import TokenTrackingEvaluator
 
 def main():
 
@@ -79,13 +70,15 @@ def main():
         "subscription_id": os.getenv("AZURE_SUBSCRIPTION_ID"),
         "resource_group_name": os.getenv("AZURE_RESOURCE_GROUP"),
         "project_name": os.getenv("AZUREAI_PROJECT_NAME"),
-    }    
+    }
+
+    token_evaluator = TokenTrackingEvaluator()
 
     # https://learn.microsoft.com/en-us/azure/ai-studio/how-to/develop/flow-evaluate-sdk
-    fluency_evaluator = TokenTrackingEvaluator(FluencyEvaluator(model_config=model_config))
-    groundedness_evaluator = TokenTrackingEvaluator(GroundednessEvaluator(model_config=model_config))
-    relevance_evaluator = TokenTrackingEvaluator(RelevanceEvaluator(model_config=model_config))
-    coherence_evaluator = TokenTrackingEvaluator(CoherenceEvaluator(model_config=model_config))
+    fluency_evaluator = token_evaluator(FluencyEvaluator(model_config=model_config))
+    groundedness_evaluator = token_evaluator(GroundednessEvaluator(model_config=model_config))
+    relevance_evaluator = token_evaluator(RelevanceEvaluator(model_config=model_config))
+    coherence_evaluator = token_evaluator(CoherenceEvaluator(model_config=model_config))
 
     data = "./responses.jsonl"  # path to the data file
 
