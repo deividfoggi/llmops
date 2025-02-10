1ad8b6c6de04d4a00d4e671d6cbfdd2e5875c644
deividfoggi <dev1@mngenvmcap380802.onmicrosoft.com>
2025-02-10 17:50:14 -0300

    Refactor old essay response handling to use a class-based approach for improved structure and maintainability

--- src/OLD_ESSAY/old_essay.py
+++ src/OLD_ESSAY/old_essay.py
@@ -1,19 +1,26 @@
 import os
 from promptflow.core import Prompty
+from promptflow.core import AzureOpenAIModelConfiguration
 
-def get_response_old_essay(essay_request, model_config):
-    print("inputs:", essay_request)
-    print("getting result...")
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    prompty_path = os.path.join(current_dir, "old_essay.prompty")
-    prompty = Prompty.load(source=prompty_path)
-    result = prompty(
-        language=essay_request["language"],
-        genre=essay_request["genre"],
-        statement=essay_request["statement"],
-        title=essay_request["title"],
-        essay=essay_request["essay"],
-        support_text=essay_request["support_text"],
-        skills=essay_request["skills"]
-    )
-    return result
+class OldEssay:
+    def __init__(self, model_config: AzureOpenAIModelConfiguration):
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.prompty_path = os.path.join(self.current_dir, "old_essay.prompty")
+        self.model_config = model_config
+
+    def __call__(self, essay_request: str) -> str:
+        print("inputs:", essay_request)
+        print("getting result...")
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        prompty_path = os.path.join(current_dir, "old_essay.prompty")
+        prompty = Prompty.load(source=prompty_path)
+        result = prompty(
+            language=essay_request["language"],
+            genre=essay_request["genre"],
+            statement=essay_request["statement"],
+            title=essay_request["title"],
+            essay=essay_request["essay"],
+            support_text=essay_request["support_text"],
+            skills=essay_request["skills"]
+        )
+        return result
