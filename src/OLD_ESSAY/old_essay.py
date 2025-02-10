df3d2d85b1a29126854ada69d1fdc0dfb40a12c1
deividfoggi <dev1@mngenvmcap380802.onmicrosoft.com>
2025-02-10 12:22:27 -0300

    Update old essay response function to use dynamic prompty path

--- src/OLD_ESSAY/old_essay.py
+++ src/OLD_ESSAY/old_essay.py
@@ -1,8 +1,11 @@
+import os
 from promptflow.core import Prompty
 
 def get_response_old_essay(essay_eval_request):
     print("inputs:", essay_eval_request)
     print("getting result...")
-    prompty = Prompty.load(source="./src/OLD_ESSAY/old_essay.prompty")
-    result = prompty(language = essay_eval_request["language"], genre = essay_eval_request["genre"], statement = essay_eval_request["statement"], title = essay_eval_request["title"], essay = essay_eval_request["essay"], support_text = essay_eval_request["support_text"], skills = essay_eval_request["skills"])
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    prompty_path = os.path.join(current_dir, "old_essay.prompty")
+    prompty = Prompty.load(source=prompty_path)
+    result = prompty(language=essay_eval_request["language"], genre=essay_eval_request["genre"], statement=essay_eval_request["statement"], title=essay_eval_request["title"], essay=essay_eval_request["essay"], support_text=essay_eval_request["support_text"], skills=essay_eval_request["skills"])
     return result
