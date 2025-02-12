27bbfe494ef1a291752c9205e93b98602d5d323a
deividfoggi <dev1@mngenvmcap380802.onmicrosoft.com>
2025-02-12 15:56:16 -0300

    Refactor essay evaluation structure by removing old implementation and introducing a new class-based flow; update input JSON and test cases accordingly

--- src/simple_essay/simple_essay.py
+++ src/simple_essay/simple_essay.py
@@ -6,22 +6,22 @@ from promptflow.core import AzureOpenAIModelConfiguration
 class OldEssay:
     def __init__(self, model_config: AzureOpenAIModelConfiguration):
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.prompty_path = os.path.join(self.current_dir, "old_essay.prompty")
+        self.prompty_path = os.path.join(self.current_dir, "simple_essay.prompty")
         self.model_config = model_config
 
     def __call__(self, essay_request: str) -> str:
         print("inputs:", essay_request)
         print("getting result...")
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        prompty_path = os.path.join(current_dir, "old_essay.prompty")
+        prompty_path = os.path.join(current_dir, "simple_essay.prompty")
         prompty = Prompty.load(source=prompty_path)
         result = prompty(
-            lingua=essay_request["lingua"],
-            genero=essay_request["genero"],
-            tema=essay_request["proposta"],
-            titulo=essay_request["tema"],
-            redacao=essay_request["redacao"],
-            material_apoio=essay_request["material_apoio"],
+            language=essay_request["language"],
+            genre=essay_request["genre"],
+            statement=essay_request["statement"],
+            title=essay_request["title"],
+            essay=essay_request["essay"],
+            support_material=essay_request["support_text"],
             skills=essay_request["skills"]
         )
         return result
--- tests/test_essay_request_router.py
+++ tests/test_essay_request_router.py
@@ -8,7 +8,7 @@ class TestEssayRequestRouter(unittest.TestCase):
     @patch('essay_request_router.OldEssay')
     @patch('essay_request_router.EssayEvaluationFlow')
     @patch('essay_request_router.AzureOpenAIModelConfiguration')
-    def test_essay_request_router_http_response(self, mock_azure, mock_essay_flow, mock_old_essay):
+    def test_essay_request_router_http_response(self, mock_azure, mock_essay_flow, mock_simple_essay):
         tester = app.test_client(self)
         # read content from input.json file
         with open('tests/input.json') as f:
